c05e2519c19f74169310f1b38119dc9e0a7b0008
mariasbl <mbc.silva@campus.fct.unl.pt>
2024-01-06 18:58:58 +0000

    Remove unused div

--- fe/app/components/BannerGrid/BannerGrid.tsx
+++ fe/app/components/BannerGrid/BannerGrid.tsx
@@ -4,18 +4,16 @@ import { BannerGridProps } from "./BannerGrid.models";
 
 const BannerGrid = ({ images }: BannerGridProps) => {
   return (
-    <div>
-      <div className="flex relative overflow-hidden">
-        <div className="flex items-center justify-ceneter min-w-max overflow-hidden gap-3 animate-autoplay-infinite">
-          <BannerGridContent images={images} />
-        </div>
-        <div className="flex items-center justify-end min-w-max overflow-hidden gap-3 animate-autoplay-infinite ml-3">
-          <BannerGridContent images={images} />
-        </div>
-        <div className="flex absolute h-full w-full bg-banner-gradient items-center p-9 md:p-12">
-          <div className="font-league-gothic whitespace-pre-line text-white uppercase text-4xl md:text-7xl">
-            {"Create your own art by\n exchanging ideas with\n other artists"}
-          </div>
+    <div className="flex relative overflow-hidden">
+      <div className="flex items-center justify-ceneter min-w-max overflow-hidden gap-3 animate-autoplay-infinite">
+        <BannerGridContent images={images} />
+      </div>
+      <div className="flex items-center justify-end min-w-max overflow-hidden gap-3 animate-autoplay-infinite ml-3">
+        <BannerGridContent images={images} />
+      </div>
+      <div className="flex absolute h-full w-full bg-banner-gradient items-center p-9 md:p-12">
+        <div className="font-league-gothic whitespace-pre-line text-white uppercase text-4xl md:text-7xl">
+          {"Create your own art by\n exchanging ideas with\n other artists"}
         </div>
       </div>
     </div>
