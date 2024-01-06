53b4af967a92b2f9ee187a6a558f1d641768c3f3
Daniela Prado <prado-pp@Danielas-MBP.lan>
2024-01-06 22:21:04 +0000

    feat: PR suggestions + fix package json script

--- fe/app/components/BulletPoint/BulletPoint.tsx
+++ fe/app/components/BulletPoint/BulletPoint.tsx
@@ -2,11 +2,11 @@ import { BulletPointProps } from "@/app/components/BulletPoint/BulletPoint.model
 
 const BulletPoint = ({ category, text }: BulletPointProps) => {
   return (
-    <div className="flex sm:gap-3 gap-2 items-baseline pt-5">
+    <div className="flex md:gap-3 gap-2 items-baseline pt-5">
       <div
-        className={`flex-shrink-0 w-2 h-2 sm:w-[10px] sm:h-[10px] bg-${category}`}
+        className={`flex-shrink-0 w-2 h-2 md:w-[10px] md:h-[10px] bg-${category}`}
       />
-      <p className="text-xs sm:text-base">{text}</p>
+      <p className="text-xs md:text-base font-lato">{text}</p>
     </div>
   );
 };
