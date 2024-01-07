29d374aa8e60045aa1bca1868be9a9f88c55d7de
CPM <129984310+catarinapm@users.noreply.github.com>
2024-01-07 19:09:49 +0000

    CDM-14/37: New ContactInfo component and Design System page (#11)
    * feat: new ContactInfo component and models
    
    * feat: new Instagram icon
    
    * feat: new Design System page
    
    * fix: revert and update classNames Divider
    
    * refactor: remove unused import Divider component
    
    * refactor: update var name
    
    * refactor: remove withe space
    
    * feat: add main bg color black and text color white
    
    * chore: update build text

---
+++ fe/app/components/ContactInfo/ContactInfo.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ContactInfoProps } from "./ContactInfo.models";
+import Divider from "../Divider/Divider";
+
+const ContactInfo = ({ email, mobilePhone, socials }: ContactInfoProps) => {
+  return (
+    <div className="flex flex-col gap-y-2 w-[290px] md:w-[439px] font-lato md:text-xl">
+      <span>{email}</span>
+      <span>
+        <b>+351</b> {mobilePhone}
+      </span>
+      <Divider />
+      <div className="grid grid-cols-2 md:grid-cols-3 items-center font-semibold md:text-base">
+        {socials.map((social) => (
+          <div key={social.text} className="flex gap-x-2 items-center">
+            {social.icon}
+            <span>{social.text}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
--- fe/app/components/Divider/Divider.tsx
+++ fe/app/components/Divider/Divider.tsx
@@ -1,12 +1,11 @@
 import { DividerProps } from "@/app/components/Divider/Divider.models";
-import clsx from "clsx";
 import React from "react";
 
 const Divider = ({ category, orientation = "horizontal" }: DividerProps) => {
   return orientation === "horizontal" ? (
-    <hr className={`w-[1px] self-stretch bg-${category ?? "white"}`} />
+    <hr className={`w-full border-${category}`} />
   ) : (
-    <div className={`w-full border-${category}`} />
+    <div className={`w-[1px] self-stretch bg-${category ?? "white"}`} />
   );
 };
 
