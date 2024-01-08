bd409b899808d471df44a8951444665dfaed1397
Daniela Prado <prado-pp@Danielas-MBP.lan>
2024-01-08 19:07:53 +0000

    fix: update font size on mobile + updated to current year

--- fe/app/components/Footer/Footer.tsx
+++ fe/app/components/Footer/Footer.tsx
@@ -2,14 +2,15 @@ import LocaleSwitcher from "@/app/components/LocaleSwitcher/LocaleSwitcher";
 import { MadeByDarwinIcon } from "@icons/MadeByDarwinIcon";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <div className="flex justify-between items-baseline">
       <div>
         <MadeByDarwinIcon />
-        <p className="font-league-gothic md:text-xl text-sm">MADE BY DARWIN</p>
+        <p className="font-league-gothic md:text-xl text-xs">MADE BY DARWIN</p>
       </div>
-      <p className="font-league-gothic md:text-xl text-sm">
-        COPYRIGHT © 2023 CREATIVE STUDIO NO FILTERS
+      <p className="font-league-gothic md:text-xl text-xs">
+        © {year} CREATIVE STUDIO NO FILTERS
       </p>
       <LocaleSwitcher />
     </div>
