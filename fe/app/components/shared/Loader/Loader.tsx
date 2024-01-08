72d0ee81ea8f1f3b30fd7c17cf32451f45cf7ec7
Daniela Prado <prado-pp@Danielas-MBP.lan>
2024-01-08 21:07:54 +0000

    fix: loader border weight

--- fe/app/components/shared/Loader/Loader.tsx
+++ fe/app/components/shared/Loader/Loader.tsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="inline-block md:h-5 md:w-5 w-3 h-3 animate-spin rounded-full border-[2px] border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-spin" />
+    <div className="inline-block md:h-5 md:w-5 w-3 h-3 animate-spin rounded-full border-[2px] md:border-[3px] border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-spin" />
   );
 };
 
