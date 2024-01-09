06754c257df5f9e64b32c3a6893f7912c16f7d2a
Daniela Prado <prado-pp@Danielas-MBP.lan>
2024-01-09 10:22:53 +0000

    feat(footer): add redirect to MADE BY DARWIN

--- fe/app/components/Footer/Footer.tsx
+++ fe/app/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import LocaleSwitcher from "@/app/components/LocaleSwitcher/LocaleSwitcher";
 import { MadeByDarwinIcon } from "@icons/MadeByDarwinIcon";
+import Link from "next/link";
 
 const Footer = () => {
   const year = new Date().getFullYear();
@@ -7,7 +8,14 @@ const Footer = () => {
     <div className="flex justify-between items-baseline">
       <div>
         <MadeByDarwinIcon />
-        <p className="font-league-gothic md:text-xl text-xs">MADE BY DARWIN</p>
+        <Link
+          href="https://madebydarwin.vercel.app/"
+          aria-label="Go to MADE BY DARWIN Website"
+          target="_blank"
+          className="font-league-gothic md:text-xl text-xs"
+        >
+          MADE BY DARWIN
+        </Link>
       </div>
       <p className="font-league-gothic md:text-xl text-xs">
         © {year} CREATIVE STUDIO NO FILTERS
