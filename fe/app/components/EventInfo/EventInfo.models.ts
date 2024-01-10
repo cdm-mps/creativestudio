ba6290c655ad555dabe007142eec8b6dd3ee343d
mariasbl <mbc.silva@campus.fct.unl.pt>
2024-01-10 19:38:16 +0000

    feat: Event Info Component

---
+++ fe/app/components/EventInfo/EventInfo.models.ts
@@ -0,0 +1,9 @@
+import { Category } from "@model/Category";
+
+export interface EventInfoProps {
+  date: string;
+  duration: string;
+  level?: string;
+  category: Category;
+  onClick: () => void;
+}
