2d03b67be53660102ef0bd8783396c7da969d9b8
mariasbl <mbc.silva@campus.fct.unl.pt>
2024-01-10 16:01:29 +0000

    RoundArrowButton Component

---
+++ fe/app/assets/icons/StickHead.tsx
@@ -0,0 +1,21 @@
+import type { SVGAttributes } from "react";
+
+export const StickHead = (props: SVGAttributes<HTMLOrSVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="17"
+      height="8"
+      viewBox="0 0 17 8"
+      fill="none"
+      {...props}
+    >
+      <path
+        d="M1.08203 1.25146L8.3081 6.25146L15.582 1.25146"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
