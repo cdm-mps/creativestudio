f4149c442c55cefcb721e88ac5ad6d56407cc077
mariasbl <mbc.silva@campus.fct.unl.pt>
2024-01-11 19:35:50 +0000

    Fix some issues

--- fe/app/components/NewsElement/NewsElement.tsx
+++ fe/app/components/NewsElement/NewsElement.tsx
@@ -11,7 +11,7 @@ const NewsElement = ({
   image,
 }: NewsElementProps) => {
   return (
-    <div className="flex w-fit group cursor-pointer mr-[12.75px] mb-[8px] md:mr-[20.5px] md:mb-[15px]">
+    <div className="flex w-fit group cursor-pointer mr-[12.75px] mb-2 md:mr-[20.5px] md:mb-[15px]">
       <div className="flex flex-col items-end mr-9">
         <DateInfo date={date} size="lg" category={category} />
         <span className="font-league-gothic uppercase text-3xl w-[134.5px] md:text-5xl md:w-[269px] text-end">
@@ -21,14 +21,14 @@ const NewsElement = ({
 
       <div className="flex relative w-fit">
         <div
-          className={`opacity-0 group-hover:opacity-30 absolute w-[175px] h-[190px] top-[8px] left-[8px] md:w-[360px] md:h-[384px] md:top-[15px] md:left-[15px] bg-${category}`}
+          className={`opacity-0 group-hover:opacity-30 absolute w-[175px] h-[190px] top-2 left-2 md:w-[360px] md:h-[384px] md:top-[15px] md:left-[15px] bg-${category}`}
         />
         <ImageElement
           {...image}
           className="w-[175px] h-[190px] md:w-[360px] md:h-[384px]"
         />
         <div
-          className={`flex items-center absolute justify-center w-[200.5px] h-[40px] bottom-[10px] -right-[12.9px] md:w-[401px] md:h-[76px] md:bottom-[20px] md:-right-[20.5px] bg-${category}`}
+          className={`flex items-center absolute justify-center w-[200.5px] h-10 bottom-2.5 -right-[12.9px] md:w-[401px] md:h-[76px] md:bottom-5 md:-right-[20.5px] bg-${category}`}
         >
           <span className="font-league-gothic uppercase text-2xl md:text-4xl">
             {highlight}
