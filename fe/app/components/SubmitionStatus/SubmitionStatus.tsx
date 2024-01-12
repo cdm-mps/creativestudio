20e1337efdc0e43bb1e523dd2b33dfd0f8a0e287
catarinapm <catarinapdematos@gmail.com>
2024-01-12 13:11:37 +0000

    fix: improve absolute rules

--- fe/app/components/SubmitionStatus/SubmitionStatus.tsx
+++ fe/app/components/SubmitionStatus/SubmitionStatus.tsx
@@ -4,30 +4,30 @@ import Divider from "@components/Divider/Divider";
 
 const SubmitionStatus = () => {
   return (
-    <div className=" relative">
+    <div className="md:w-[577px] w-[290px]">
       <div className="w-full text-center">
         <span className="md:text-[78px] text-3xl font-league-gothic">
           Inscrição Submetida!
         </span>
       </div>
-
-      <div className="absolute text-sm md:text-lg flex flex-col md:gap-5 md:mt-9 gap-3 mt-5">
+      <div className="relative text-sm md:text-lg flex flex-col md:gap-5 md:mt-9 gap-3 mt-5">
         <p>
           Obrigado por nos teres escolhido. Em breve receberás mais informação
           sobre este <b>Creative Workshop</b>.
         </p>
         <p> Vemos-nos em breve!</p>
         <p className="italic">Creative Studio</p>
-      </div>
 
-      <ImageElement
-        src={IconCreativeStud}
-        objectPosition="center"
-        className=" w-24 h-24 md:w-44 md:h-44 ml-auto md:mt-6 mt-3"
-        alt={"IconCreativeStud"}
-      />
-      <div className="md:w-[577px] md:mt-6 w-[277px] mt-3">
-        <Divider />
+        <Divider className="w-fit mt-3" />
+
+        <div className="absolute right-0">
+          <ImageElement
+            src={IconCreativeStud}
+            objectPosition="center"
+            className="w-24 h-24 md:w-44 md:h-44 md:mt-6 mt-3"
+            alt={"IconCreativeStud"}
+          />
+        </div>
       </div>
     </div>
   );
