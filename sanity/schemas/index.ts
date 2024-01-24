a3cc64f968f3afc26200022fa984379cbc03f9af
Daniela Prado <prado-pp@Danielas-MBP.lan>
2024-01-24 10:27:03 +0000

    feat: Add Sponsor schema

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -2,6 +2,7 @@ import comment from './comment'
 import event from './event'
 import imageElement from './imageElement'
 import mentor from './mentor'
+import sponsor from './sponsor'
 import teamElement from './teamElement'
 
-export const schemaTypes = [teamElement, imageElement, mentor, event, comment]
+export const schemaTypes = [teamElement, imageElement, mentor, event, comment, sponsor]
