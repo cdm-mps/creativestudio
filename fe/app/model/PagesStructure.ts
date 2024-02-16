1a9892a3b4e7e97b4a7ddb0a2bbe8dbd49a95b4b
mariasbl <mbc.silva@campus.fct.unl.pt>
2024-02-16 12:16:10 +0000

    feat: Business Workshops Page

--- fe/app/model/PagesStructure.ts
+++ fe/app/model/PagesStructure.ts
@@ -18,6 +18,36 @@ export interface CreativeWorkshopsPageStructure {
   elementDescription: Record<Category, Record<Locales, string>>;
 }
 
+export interface BusinessWorkshopsPageStructure {
+  isWIP?: boolean;
+  highlight: Record<Locales, string>;
+  description: Record<Locales, string>;
+  workshops: {
+    title: Record<Locales, string>;
+    description: Record<Locales, string>;
+    image: {
+      _type: string;
+      asset: {
+        _ref: string;
+        _type: string;
+      };
+    };
+    subcategories?: Record<Locales, string>[];
+  }[];
+}
+
+export interface ModalPageStructure {
+  title: Record<Locales, string>;
+  beginner: {
+    level_title: Record<Locales, string>;
+    level_description: Record<Locales, string>;
+  };
+  intermediate: {
+    level_title: Record<Locales, string>;
+    level_description: Record<Locales, string>;
+  };
+}
+
 export const PagesStructure: Record<
   Category | Page,
   { section: (Category | SubCategory | string)[]; hasAreaOfInsterest?: boolean }
