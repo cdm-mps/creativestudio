8e463cb91f4fbb97f5a53d2ddb938f73a6df9930
Daniela Prado <93549975+danielaprado@users.noreply.github.com>
2024-02-17 14:47:43 +0000

    CDM-74: Create Contacts Page (#54)
    * feat: Contacts Page mocked
    
    * feat: Integrate Sanity content
    
    * fix: Category Element category name

--- fe/app/[locale]/layout.tsx
+++ fe/app/[locale]/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   if (!locales.includes(locale)) notFound();
 
   return (
-    <html lang={locale} className="text-white bg-background scroll-smooth">
+    <html lang={locale} className="scroll-smooth bg-background text-white">
       <NextIntlClientProvider messages={messages}>
         <body className="overflow-x-hidden">
           <ConfigProvider
@@ -53,7 +53,7 @@ export default function RootLayout({
             }}
           >
             <Navbar />
-            <div className="md:mt-32 my-24 md:min-h-[calc(100vh-190px)] min-h-[calc(100vh-139px)]">
+            <div className="my-24 min-h-[calc(100vh-139px)] md:mt-32 md:min-h-[calc(100vh-190px)]">
               {children}
             </div>
             <Footer />
