ba3e1db07ff9127462027d2b6bda995db4bb46c9
catarinapm <catarinapdematos@gmail.com>
2024-02-21 11:06:41 +0000

    feat: create Calendar Skeleton

--- fe/app/[locale]/calendar/page.tsx
+++ fe/app/[locale]/calendar/page.tsx
@@ -9,6 +9,7 @@ import { urlFor } from "@/client";
 import ArrowTitle from "@components/ArrowTitle/ArrowTitle";
 import Calendar from "@components/Calendar/Calendar";
 import MentorEventBar from "@components/MentorEventBar/MentorEventBar";
+import Skeleton from "@components/Skeleton/Skeleton";
 import Title from "@components/Title/Title";
 import { NotFoundBanner } from "@components/shared/NotFoundBanner/NotFoundBanner";
 import { Locales } from "@model/Locales";
@@ -16,6 +17,17 @@ import { formatDate } from "@utils/date/formatDate";
 import { isDateInPast } from "@utils/date/isDateInPast";
 import { useLocale, useTranslations } from "next-intl";
 import { useEffect, useState } from "react";
+
+const CalendarPageSkeleton = () => {
+  return (
+    <div className="mx-40 flex flex-col gap-10">
+      <Skeleton height={72} width={229} />
+      <Skeleton height={986} />
+      <Skeleton width={190} height={206} />
+      <Skeleton height={92} />
+    </div>
+  );
+};
 
 export default function CalendarPage() {
   const t = useTranslations();
@@ -82,11 +94,11 @@ export default function CalendarPage() {
   }, [selectedDate]);
 
   if (!pageContent) {
-    return;
+    return <CalendarPageSkeleton />;
   }
 
   return (
-    <main className="mx-40 flex flex-col gap-14">
+    <main className="mx-40 flex flex-col gap-10">
       <Title title={t("Menu.calendar")} category="businessWorkshops" />
       <div className="mx-20">
         <Calendar
