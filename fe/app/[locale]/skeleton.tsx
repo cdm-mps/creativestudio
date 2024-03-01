a94a768988b7bb7876e061cd66cb5c8f561befdf
Daniela Prado <danielahortaprado@gmail.com>
2024-03-01 09:23:28 +0000

    feat: Home page mobile 1st draft

--- fe/app/[locale]/skeleton.tsx
+++ fe/app/[locale]/skeleton.tsx
@@ -6,23 +6,23 @@ export const HomePageSkeleton = () => {
     <>
       <div
         id="news-slider-section"
-        className="mx-40 flex flex-col gap-14 pt-24"
+        className="mx-6 flex flex-col gap-14 pt-8 md:mx-40 md:pt-24"
       >
         <Skeleton width={250} height={206} />
         <Skeleton height={384} />
       </div>
-      <div className="mt-[139px] w-full pt-[100px]">
-        <div className="mx-40">
+      <div className="mt-32 w-full md:mt-[139px] md:pt-[100px]">
+        <div className="mx-6 md:mx-40">
           <Skeleton width={250} height={206} />
         </div>
-        <div className="mx-40 pt-48">
+        <div className="mx-6 pt-16 md:mx-40 md:pt-48">
           <Skeleton height={494} />
         </div>
       </div>
-      <div className="mx-40 my-48 text-center">
+      <div className="mx-6 pt-16 text-center md:mx-40 md:my-48">
         <Skeleton height={72} />
       </div>
-      <div className="mx-40 mb-[104px]">
+      <div className="mx-6 pt-16 md:mx-40 md:mb-[104px]">
         <Skeleton height={115} />
       </div>
     </>
