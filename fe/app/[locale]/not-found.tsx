9b70fe57cc076075a8c848f70b835d2f13de413d
catarinapm <catarinapdematos@gmail.com>
2024-03-03 02:21:46 +0000

    fix: without menu options

--- fe/app/[locale]/not-found.tsx
+++ fe/app/[locale]/not-found.tsx
@@ -10,29 +10,30 @@ export default function NotFound() {
   const { push } = useRouter();
   const t = useTranslations("NotFound");
   return (
-    <div className=" flex h-[calc(100vh-290px)] flex-col items-center justify-center gap-40 font-league-gothic">
-      <div className="mt-32 flex flex-col items-center justify-center gap-10">
+    <div className=" flex h-[calc(100vh-290px)] flex-col items-center justify-center gap-20 font-league-gothic">
+      <div className="flex flex-col items-center justify-center gap-5">
         <IconTitle
           title={t("title")}
           category="businessWorkshops"
           mode="exclamation"
         />
         <div className="text-lg md:text-3xl">{t("subtitle")}</div>
-        <Button
-          category={"businessWorkshops"}
-          label={t("button")}
-          onClick={() => {
-            push(`/`);
-          }}
-        />
       </div>
-      <div className="flex items-center justify-center ">
+      <Button
+        category={"businessWorkshops"}
+        label={t("button")}
+        onClick={() => {
+          push(`/`);
+        }}
+      />
+
+      {/* <div className="flex items-center justify-center ">
         <CategoryElement category="editions" size="small" />
         <CategoryElement category="masterclasses" size="small" />
         <CategoryElement category="creativeTalks" size="small" />
         <CategoryElement category="workshops" size="small" />
         <CategoryElement category="artisticResidences" size="small" />
-      </div>
+      </div> */}
     </div>
   );
 }
