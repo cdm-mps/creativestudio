a5461a8273791da6643dd79a90ef9e938c326bbc
mariasbl <mbc.silva@campus.fct.unl.pt>
2024-04-09 21:16:59 +0000

    CDM-94: Previous Events & Sold Out Events (#69)
    * Previous Events & Sold Out Events
    
    * Fix build error
    
    * Fix soldout events

--- fe/app/components/Tag/Tag.tsx
+++ fe/app/components/Tag/Tag.tsx
@@ -1,9 +1,22 @@
+import clsx from "clsx";
 import { TagProps } from "./Tag.models";
 
-export const Tag = ({ label, category }: TagProps) => {
+export const Tag = ({ label, category, size }: TagProps) => {
   return (
-    <div className={`flex h-fit rounded-lg border-2 border-${category} p-2`}>
-      <span className={`font-league-gothic text-xl uppercase text-${category}`}>
+    <div
+      className={clsx(
+        `flex h-fit rounded-lg border-2 p-2`,
+        category ? `border-${category}` : "border-white",
+      )}
+    >
+      <span
+        className={clsx(
+          `font-league-gothic uppercase text-${category}`,
+          category ? `text-${category}` : "text-white",
+          size === "small" && "text-sm md:text-xl",
+          size === "large" && "text-xl md:text-4xl",
+        )}
+      >
         {label}
       </span>
     </div>
