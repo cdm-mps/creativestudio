1e5da0242c3dd7187f90f031fcf2769dc0929ad1
Daniela Prado <prado-pp@Danielas-MBP.home>
2024-05-16 11:01:26 +0100

    feat: Creative Workshops skeleton

--- fe/app/[locale]/creative-workshops/skeleton.tsx
+++ fe/app/[locale]/creative-workshops/skeleton.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import Skeleton from "@components/Skeleton/Skeleton";
 
 export const CreativeWorkshopsSkeleton = () => (
-  <div className="mx-40 flex flex-col">
-    <Skeleton height={72} width={440} />
-    <Skeleton height={42} width={300} className="mt-14" />
-    <Skeleton height={355} className="mb-14 mt-12" />
+  <div className="mx-12 flex flex-col md:mx-40">
+    <Skeleton height={72} width={240} />
+    <Skeleton height={42} width={300} className="mt-7 md:mt-14" />
+    <Skeleton height={355} className="mb-7 mt-6 md:mb-14 md:mt-12" />
     <div className="flex flex-col items-center">
-      <Skeleton height={72} width={414} className="mb-14 mt-6" />
-      <Skeleton height={699} width={1049} className="mb-14 mt-12" />
+      <Skeleton height={72} className="mb-7 mt-3 md:mb-14 md:mt-6" />
+      <Skeleton height={699} className="mb-7 mt-6 md:mb-14 md:mt-12" />
     </div>
   </div>
 );
