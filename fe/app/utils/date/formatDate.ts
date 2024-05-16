eeabf06959567391e28d03736bb1502366cf9dc8
mariasbl <mbc.silva@campus.fct.unl.pt>
2024-05-16 17:25:10 +0000

    fixes

--- fe/app/utils/date/formatDate.ts
+++ fe/app/utils/date/formatDate.ts
@@ -1,11 +1,9 @@
-import { useLocale } from "next-intl";
-
 export const formatDate = (
   value: string,
+  locale: string,
   monthType?: Intl.DateTimeFormatOptions["month"],
 ): { day: string; month: string; year: string } => {
   const date = new Date(value);
-  const locale = useLocale();
 
   return {
     day: String(date.getDate()).padStart(2, "0"),
