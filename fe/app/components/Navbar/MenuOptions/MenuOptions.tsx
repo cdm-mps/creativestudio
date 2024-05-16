503c4d132afff076936d72081306b9cb4e97c3a9
mariasbl <mbc.silva@campus.fct.unl.pt>
2024-05-16 18:24:07 +0000

    commit

--- fe/app/components/Navbar/MenuOptions/MenuOptions.tsx
+++ fe/app/components/Navbar/MenuOptions/MenuOptions.tsx
@@ -12,7 +12,7 @@ const MenuOptions = ({ onClick }: MenuOptionsProps) => {
           href={href}
           aria-label={label}
           onClick={onClick}
-          className="z-50 uppercase max-lg:w-full max-lg:border-b max-lg:border-t max-lg:py-4 max-lg:text-center max-lg:hover:bg-businessWorkshops lg:hover:underline lg:hover:decoration-businessWorkshops"
+          className="uppercase max-lg:w-full max-lg:border-b max-lg:border-t max-lg:py-4 max-lg:text-center max-lg:hover:bg-businessWorkshops lg:hover:underline lg:hover:decoration-businessWorkshops"
         >
           {text}
         </Link>
